4300f7664f5756ca801a7abd93589a51f3e17611
explainthis <hello@explainthis.io>
2023-03-24 00:20:57 +0800

    Update storage.py

--- src/storage.py
+++ src/storage.py
@@ -5,15 +5,18 @@ import datetime
 class FileStorage:
     def __init__(self, file_name):
         self.fine_name = file_name
+        self.history = {}
 
     def save(self, data):
-        with open(self.fine_name, 'a+', newline='') as f:
-            json.dump(data, f)
+        self.history.update(data)
+        with open(self.fine_name, 'w', newline='') as f:
+            json.dump(self.history, f)
 
     def load(self):
         with open(self.fine_name, newline='') as jsonfile:
             data = json.load(jsonfile)
-        return data
+        self.history = data
+        return self.history
 
 
 class MongoStorage:
